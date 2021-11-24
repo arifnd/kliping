bd97768c506dc1c7fdf00bd7be1d0b8054240d79
Ari Effendi <ari@zerosix.id>
2021-11-25 00:09:06 +0700

    Create migration

---
+++ database/migrations/2021_11_22_213413_create_sources_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedTinyInteger('type_id');
+            $table->string('name');
+            $table->string('url');
+            $table->string('custom_schema')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sources');
+    }
+}
