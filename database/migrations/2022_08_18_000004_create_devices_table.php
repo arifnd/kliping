d313ffcf0d3c56fc2ad5f0d99577465e049db13d
Ari Effendi <ari@arped.id>
2022-08-19 21:52:58 +0800

    Create device model and migration

---
+++ database/migrations/2022_08_18_000004_create_devices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->string('id', 6)->unique();
+            $table->unsignedBigInteger('last_news');
+            $table->timestamps();
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
