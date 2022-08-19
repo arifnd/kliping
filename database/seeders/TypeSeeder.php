a0b06ab4b04ad7deb4d726871a366ffeb41d2094
Ari Effendi <ari@arped.id>
2022-08-19 22:07:16 +0800

    First commit

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('types')->insert([
+            ['id' => 1, 'type' => 'feed'],
+            ['id' => 2, 'type' => 'web'],
+        ]);
+    }
+}
