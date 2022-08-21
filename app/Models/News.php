7758803e0ab0424fc3c4f243f35cf95f59df1552
Ari Effendi <ari@arped.id>
2022-08-21 12:33:02 +0800

    Add guarded attributes

--- app/Models/News.php
+++ app/Models/News.php
@@ -10,6 +10,17 @@ class News extends Model
     use HasFactory;
 
     /**
+    * The attributes that aren't mass assignable.
+    *
+    * @var array
+    */
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
      * Get the source that owns the news.
      */
     public function source()
