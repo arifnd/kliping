3f8d834504e3539b1766385174f2af2712ecd405
Ari Effendi <ari@arped.id>
2023-11-01 01:35:24 -0400

    First commit

---
+++ database/migrations/2023_11_01_051737_create_sources_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->string('url');
+            $table->dateTime('next_update', $precision = 0)->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sources');
+    }
+};
